0d676e8c19be89668e45053599849ad7f2899a16
Melxi <magomed.futijev@gmail.com>
2019-07-23 10:11:16 +0300

    6.8: anekdootit, step6

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,38 +1,13 @@
 import React from 'react';
-import { voteAnecdote, addAnecdote } from './reducers/anecdoteReducer';
-import AnecdoteForm from './components/AnecdoteForm';
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList';
 
 const App = (props) => {
-  const anecdotes = props.store
-    .getState()
-    .sort((a, b) => b.votes - a.votes)
-
-  const vote = (id) => {
-    props.store.dispatch(voteAnecdote(id))  
-  }
-
-  const add = (event) => {
-    event.preventDefault()
-    const content = event.target.add.value
-    props.store.dispatch(addAnecdote(content))
-    event.target.add.value = ''
-  }
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <AnecdoteForm add={add} />
+      <AnecdoteList store={props.store} />
+      <AnecdoteForm store={props.store} />
     </div>
   )
 }
--- redux-anecdotes/src/components/AnecdoteForm.js
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import { addAnecdote } from '../reducers/anecdoteReducer'
 
-const AnecdoteForm = ({ add }) => {    
-    return (
-        <div>
-            <h2>create new</h2>
-            <form onSubmit={add}>
-                <div><input name="add"/></div>
-                <button>create</button>
-            </form>
-        </div>
-    )
+const AnecdoteForm = (props) => {
+  const add = (event) => {
+    event.preventDefault()
+    const content = event.target.add.value
+    props.store.dispatch(addAnecdote(content))
+    event.target.add.value = ''
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={add}>
+        <div><input name="add"/></div>
+        <button>create</button>
+      </form>
+    </div>
+  )
 }
 
 export default AnecdoteForm
